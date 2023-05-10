445dbd7b425120eb1e506533ba8fef427b996661
mablakulova <abmakhliyo@gmail.com>
2023-05-10 18:40:06 +0500

    Implemented the ECHO command

--- redis-clone/redis-clone/Server.cs
+++ redis-clone/redis-clone/Server.cs
@@ -28,8 +28,50 @@ public class Server
 
         while ((bytesRead = socket.Receive(buffer)) > 0)
         {
-            byte[] response = Encoding.ASCII.GetBytes("+PONG\r\n");
-            socket.Send(response);
+            var command = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            var arguments = DecodeCommand(command);
+
+            if (arguments[0].ToUpper() == "PING")
+            {
+                socket.Send(Encoding.ASCII.GetBytes("+PONG\r\n"));
+            }
+            else if (arguments[0].ToUpper() == "ECHO")
+            {
+                var resp = arguments[1];
+                socket.Send(Encoding.ASCII.GetBytes($"+{resp}\r\n"));
+            }
+            else
+            {
+                socket.Send(Encoding.UTF8.GetBytes("+UNKNOWN\r\n"));
+            }
         }
+    }
+
+    private List<string> DecodeCommand(string command)
+    {
+        if (command[0] != '*')
+        {
+            throw new ArgumentException("Invalid command format.");
+        }
+
+        var lines = command.Split(new[] { "\r\n" }, StringSplitOptions.None);
+        var numberOfArguments = int.Parse(lines[0].Substring(1));
+        var arguments = new List<string>();
+
+        for (int i = 1; i < lines.Length; i++)
+        {
+            if (lines[i].StartsWith("$"))
+            {
+                arguments.Add(lines[i + 1]);
+                i++;
+            }
+        }
+
+        if (arguments.Count != numberOfArguments)
+        {
+            throw new ArgumentException("Invalid command format.");
+        }
+
+        return arguments;
     }
 }
